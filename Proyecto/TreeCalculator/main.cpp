d0bfc9702e2ab188db1b596bb08a0ece5bd32c29
STALIN RIVERA VEGA <55595831+Stalin0@users.noreply.github.com>
2021-08-04 01:32:51 -0500

    Login

--- Proyecto/TreeCalculator/main.cpp
+++ Proyecto/TreeCalculator/main.cpp
@@ -8,8 +8,23 @@
  * @date 05/07/2021
  * @author Diego Jimenez
  */
+ #define BACKSPACE 8
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string>
+#include <cstdlib>
+#include <conio.h>
+#include <fstream>
+#define USUARIO "user1"
+#define PASSWORD "500"
+#define SUPER "BackSquad"
+#define PASS "SOFTWARE"
+#define ENTER 13
+#include <iostream>
+//#include <SFML/Graphics.hpp>
 #include "stack.h"
 #include <string>
 #include "opstr.h"
@@ -17,7 +32,65 @@
 #include "tnode.h"
 #include "opstree.h"
 
+using namespace std;
+struct usuario
+{
+    string nombre;
+    string contrasenia;
+} personas[100];
+
 int main(int, char**) {
+	
+	    string usuario, password;
+
+    int cont = 0;
+    bool ingresa = false;
+
+    do
+    {
+        system("cls");
+        cout << "\t\t\t*******BIENVENIDO AL SISTEMA********";
+        cout << "\n\n\tIngrese el Usuario: ";
+        getline(cin, usuario);
+
+        char caracter;
+
+        cout << "\tClave: ";
+        caracter = getch();
+
+        password = "";
+
+        while (caracter != ENTER)
+        {
+
+            if (caracter != BACKSPACE)
+            {
+                password.push_back(caracter);
+                cout << "*";
+            }
+            caracter = getch();
+        }
+
+        if (usuario == SUPER && password == PASS)
+        {
+            ingresa = true;
+        }
+        else
+        {
+
+        }
+    } while (ingresa == false && cont < 3);
+
+    if (ingresa == false)
+    {
+        cout << "\n\tPor varios errores, el sistema se cerrara en cualquier momento \n";
+    }
+    else if (usuario == "BackSquad" && password == "SOFTWARE")
+    {
+        cout << "\n\n\tSea Bienvenido\n\n";
+        system("pause");
+    }
+	
     std::cout << "Programa para cambiar notacion prefija, postfija y funcional\n";
     std::cout << "Ingrese una expresion matematica" <<std::endl;
     std::string str;
@@ -51,4 +124,30 @@ int main(int, char**) {
     
     
     system("pause");
+    cin.get();
+    return 0;
+}
+
+int mostrar()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (personas[i].contrasenia != "")
+        {
+            cout << personas[i].nombre << endl;
+            cout << personas[i].contrasenia << endl;
+        }
+    }
+}
+
+int quitar_usuario(int usuario1)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (usuario1 == i)
+        {
+            personas[usuario1].nombre = " ";
+            personas[usuario1].contrasenia = " ";
+        }
+    }
 }
--- Proyecto/TreeCalculator/tree.h
+++ Proyecto/TreeCalculator/tree.h
@@ -5,18 +5,149 @@ template <typename A>
 class Tree{
     private:
         TNode<A>* root = nullptr;
+        ofstream archivo;
         // int size = 0;
     public:
         Tree() = default;
+        bool Vacio();
+        void imprimirArbol(Nodo<A>*arbol);
+        Node<T>* crearNuevoNodo(A);
         void insert(A dat);
+        TNode<A>* getArbol();
+        void setArbol(Node<A>*);
+        void displayArbol();
+        void eliminarNodo(Node<A>* nodoElimina);
+        void mostrarArbol(Node<A>* arbol, int cont);
         TNode<A>* search(A dat);
+        bool buscar(int  valor);
         void eraser(A dat);
         TNode<A>* getRoot();
-        
+        void eliminarNodoPri(A, Node<A>*);
+        void eliminarCoincidencias();
+        Node<A>* getmin(Node<A>*raiz);
+        int encontrarMinimo(Node<A>*);
+        void removerEntre(Node<A>*,Node<A>*, bool);
+        void generarGrafico();
+        void graficarArbol(Node<A>*nodo);
+
+
 };
+template <typename A>
+bool Arbol<A>::Vacio(){
+    return (raiz==nullptr);
+}
+template <typename A>
+Node<A>*Arbol<A>::crearNuevoNodo(A valor){
+    Node<A>* temp=new Node<A>();
+    temp->setValor(valor);
+    return temp;
+}
+template <typename A>
+Node<A>* Arbol<A>::getArbol(){
+    return this->root;
+}
+template <typename A>
+void Arbol<A>::setArbol(Nodo<A>* _arbol){
+    this->root=_arbol;
+}
+template <typename A>
+void Arbol<A>::displayArbol(){
+    imprimirArbol(root);
+    cout<<endl<<endl;
+    cout<<endl<<endl;
+    mostrarArbol(root, 0);
+}
+template <typename A>
+void Arbol<A>::imprimirArbol(Node<A>* arbol){
+    if(arbol!==NULL){
+        imprimirArbol(arbol->getNodoIzquierdo());
+        cout<<" "<<arbol->getValor()<<endl" ";
+        imprimirArbol(arbol->getNodoDerecho());
+    }
+}
+template<typename A>
+Node<A>* arbol<A>::buscar(int Valor, Node<A> *temp){
+
+    if(temp != nullptr)
+    {
+        if(Valor==temp->getValor())
+            return temp;
+        if(Valor<temp->getValor())
+            return buscar(Valor, temp->getNodoIzquierdo());
+        else
+            return buscar(Valor, temp->getNodoDerecho());
+
+    }else{
+        return nullptr;
+    }
+}
+
+template<typename A>
+bool Arbol<A>::buscar(int valor){
+Node<A>* temp = buscar(valor, root);
+if(temp==nullptr){
+    return false;
+}
+else if(temp->getValor()==valor){
+    return true;
+}else{
+    return false;
+}
+}
+template<typename A>
+void Arbol<A>::eliminar(A dato){
+    eliminarNodoPri(dato, root);
+}
+template <typename A>
+void Arbol<A>::eliminarNodoPri(A dato,Node<A>*privado){
+    if(root !=nullptr){
+        if(root->getValor() == dato)
+            eliminarCoincidencias();
+        else{
+            if(dato < privado->gatValor()&&privado->primero->getNodoIzquierda() !=nullptr){
+               privado->getNodoIzquierdo()->getValor()==dato ? removeEntre(privado, getNodoIzquierdo(), true): elominarNodoPri(dato, privado->getNodoIzquierdo());
+
+            }
+            else
+            if(dato > privado->getValor()&& privado->getNodoDerecha()!=nullptr){
+                privado->getNodoDerecho()->getValor() == dato ? removeEntre(privado, privado->getNodoDerecho(),false) : eliminarNodoPri(dato, privado->getNodoDerecho());
+            }
+            else
+                cout<< "No se ha encontrado el dato en el arbol"<<endl;
+        }
+    }else
+    cout<<"El arbol esta vacio"<<endl;
+}
+
+template<typename A>
+void Arbol<A>::eliminarCoincidencias(){
+    if(root !=nullptr){
+        Node<A>* datoPrivado = root;
+        int datoRoot = root->getValor();
+        int datoMinimo;
+        if (root ->getNodoIzquierdo()==nullptr&&root->getNodoDerecho()==nullptr){
+           delete datoPrivado;
+           root = nullptr;
+        }
+        else
+        if(root->getNodoIzquierdo()!=nullptr&&root->getNodoDerecho()==nullptr){
+            root=root->getNodoIzquierdo();
+            datoPrivado->detNodoIzquierdo(nullptr);
+            delete datoPrivado;
+        }else
+        if(root->getNodoIzquierdo()==nullptr&&root->getNodoDerecho()!=nullptr){
+           datoMinimo = encontrarMinimo(root->getNodoDerecho());
+            eliminarNodoPri(datoMinimo, root);
+            root->setValor(datoMinimo);
+        }
+    }else
+    cout<<"!!ERROR"<<endl;
+
+}
 
 template <typename A>
 void Tree<A>::insert(A dat){
+
 
 }
 
